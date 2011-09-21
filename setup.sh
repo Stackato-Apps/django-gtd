a9428543503b15a3c83b36b737c20b823529d798
Sridhar Ratnakumar <sridharr@activestate.com>
2011-09-21 11:34:32 -0700

    django-gtd: using multiple commands in post-staging pinax-social: add post-staging hook

--- setup.sh
+++
@@ -1,5 +0,0 @@
-python manage.py syncdb --noinput
-python manage.py migrate --noinput
-# Create the admin user
-python manage.py createsuperuser --username=admin --email=admin@mydomain.com --noinput
-python manage.py changepassword2 admin secret123
