ac12e684e90d93eb4dd7c21a2e39725faa05727e
Matthew Fisher <matt.fisher6@gmail.com>
2013-03-26 09:49:39 -0700

    updated deprecated functions to Django 1.5

--- gtd/views/objects.py
+++ gtd/views/objects.py
@@ -1,26 +1,28 @@
 from gtd.models import Thing
-from django.views.generic.list_detail import object_list, object_detail
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 def thing_list(request):
     thing_list = Thing.objects.all()
-    return object_list(request, queryset=thing_lists)
+    return ListView(request, queryset=thing_lists)
 
 def thing_detail(request, id):
     thing_detail = Thing.objects.get(id)
-    return object_detail(request, queryset=thing_detail)
+    return DetailView(request, queryset=thing_detail)
 
 def context_list(request):
     context_list = Context.objects.all()
-    return object_list(request, queryset=context_lists)
+    return ListView(request, queryset=context_lists)
 
 def context_detail(request, id):
     context_detail = Context.objects.get(id)
-    return object_detail(request, queryset=context_detail)
+    return DetailView(request, queryset=context_detail)
 
 def project_list(request):
     project_list = Project.objects.all()
-    return object_list(request, queryset=project_lists)
+    return ListView(request, queryset=project_lists)
 
 def project_detail(request, id):
     project_detail = Project.objects.get(id)
-    return object_detail(request, queryset=project_detail)
+    return DetailView(request, queryset=project_detail)
